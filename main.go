f7fa4239fb3326cbe53ea80a99efd444a0345b51
Tyler Bialoblocki <hamburgertrain@users.noreply.github.com>
2024-01-19 19:49:45 -0500

    Reorder main, modify sim

--- main.go
+++ main.go
@@ -12,14 +12,8 @@ import (
 )
 
 func main() {
-	log.Println("Initializing connection to ELM327 device...")
-	elm327.ContactElm327Device()
-	log.Println("Connection initialized")
-
 	log.Println("Initializing connection to i2c display...")
-	// Create new connection to I2C bus on 2 line with address 0x27
 	i2cConnection := display.Initialize()
-	// Free I2C connection on exit
 	defer i2cConnection.Close()
 	log.Println("Connection initialized")
 
@@ -29,25 +23,35 @@ func main() {
 	log.Println("Display reset")
 
 	log.Println("Writing to display...")
-	display.LcdDisplayString(i2cConnection, "Hello world!", 1, 0)
+	display.LcdDisplayString(i2cConnection, "----BoostPi-----", 1, 0)
+	display.LcdDisplayString(i2cConnection, "Now Loading.....", 2, 0)
 	log.Println("Writing done.")
 
 	time.Sleep(5 * time.Second)
+
+	log.Println("Initializing connection to ELM327 device...")
+	elm327.ContactElm327Device()
+	log.Println("Connection initialized")
 
 	log.Println("Clearing display...")
 	display.Clear(i2cConnection)
 	log.Println("Clear")
 
-	log.Println("Simulating boost...")
-	SimulateBoost(i2cConnection)
+	for true {
+		log.Println("Simulating boost...")
+		SimulateBoost(i2cConnection, 7, 18)
+		time.Sleep(5 * time.Second) // take a lil break
+		SimulateBoost(i2cConnection, 15, 21)
+		break
+	}
 
 	log.Println("Turning display off")
 	display.TurnOff(i2cConnection)
 }
 
 // Simulate boost climbing very crudely
-func SimulateBoost(connection *i2c.I2C) {
-	for i := 0; i < 30; i++ {
+func SimulateBoost(connection *i2c.I2C, start int, end int) {
+	for i := start; i < end; i++ {
 		displayString := strconv.Itoa(i) + " psi"
 		display.LcdDisplayString(connection, displayString, 1, 0)
 		time.Sleep(1 * time.Second)
