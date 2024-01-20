72ba5e23c70019f1dc527d0840a3b876062363dc
Tyler Bialoblocki <hamburgertrain@users.noreply.github.com>
2024-01-19 19:58:23 -0500

    Reorganize elm327 package

--- elm327/elm327.go
+++ elm327/elm327.go
@@ -10,7 +10,7 @@ import (
 var ELM327_DEVICE_LOCATION string = "/dev/ttyUSB0"
 var ELM327_DEBUG bool = true
 
-// Establish contact with an ELM327 OBD-II reader
+// Establish contact with an ELM327 OBD-II reader nd read some stats
 func ContactElm327Device() {
 	dev, err := elmobd.NewDevice(ELM327_DEVICE_LOCATION, ELM327_DEBUG)
 	if err != nil {
@@ -26,12 +26,40 @@ func ContactElm327Device() {
 
 	log.Println("Device has version", version)
 
-	rpm, err := dev.RunOBDCommand(elmobd.NewEngineRPM())
+	GetEngineRpm(dev)
+	GetIntakeManifoldPressure(dev)
+	GetMassAirflowRate(dev)
+}
 
+// Get engine rpm from OBD device
+func GetEngineRpm(dev *elmobd.Device) {
+	rpm, err := dev.RunOBDCommand(elmobd.NewEngineRPM())
 	if err != nil {
 		log.Println("Failed to get rpm", err)
 		return
 	}
 
-	log.Printf("Engine spins at %s RPMs\n", rpm.ValueAsLit())
+	log.Printf("Engine spinning at %s RPMs\n", rpm.ValueAsLit())
+}
+
+// Get intake manifold pressure from OBD device
+func GetIntakeManifoldPressure(dev *elmobd.Device) {
+	imaf, err := dev.RunOBDCommand(elmobd.NewIntakeManifoldPressure())
+	if err != nil {
+		log.Println("Failed to get imaf", err)
+		return
+	}
+
+	log.Printf("Intake Manifold Pressure is %s\n", imaf.ValueAsLit())
+}
+
+// Get mass airflow rate from OBD device
+func GetMassAirflowRate(dev *elmobd.Device) {
+	mafr, err := dev.RunOBDCommand(elmobd.NewMafAirFlowRate())
+	if err != nil {
+		log.Println("Failed to get mafr", err)
+		return
+	}
+
+	log.Printf("Mass airflow rate is %s\n", mafr.ValueAsLit())
 }
