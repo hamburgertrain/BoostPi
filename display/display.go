c698b343a48fb202ae09b5a8743bc5a0411a5032
Tyler Bialoblocki <hamburgertrain@users.noreply.github.com>
2024-01-20 15:47:12 -0500

    Make global vars more go-like

--- display/display.go
+++ display/display.go
@@ -23,61 +23,61 @@ import (
 )
 
 // i2c bus (0 -- original Pi, 1 -- Rev 2 Pi)
-var I2CBUS int = 1
+var i2cBus int = 1
 
 // LCD Address
-var ADDRESS uint8 = 0x27
+var i2cAddress uint8 = 0x27
 
 // Commands
-var LCD_CLEARDISPLAY uint8 = 0x01
-var LCD_RETURNHOME uint8 = 0x02
-var LCD_ENTRYMODESET uint8 = 0x04
-var LCD_DISPLAYCONTROL uint8 = 0x08
-var LCD_CURSORSHIFT uint8 = 0x10
-var LCD_FUNCTIONSET uint8 = 0x20
-var LCD_SETCGRAMADDR uint8 = 0x40
-var LCD_SETDDRAMADDR uint8 = 0x80
+var lcdClearDisplay uint8 = 0x01
+var lcdReturnHome uint8 = 0x02
+var lcdEntryModeSet uint8 = 0x04
+var lcdDisplayControl uint8 = 0x08
+var lcdCursorShift uint8 = 0x10
+var lcdFunctionSet uint8 = 0x20
+var lcdSetCGRamAddress uint8 = 0x40
+var lcdSetDDRAMAddress uint8 = 0x80
 
 // Flags for display entry mode
-var LCD_ENTRYRIGHT uint8 = 0x00
-var LCD_ENTRYLEFT uint8 = 0x02
-var LCD_ENTRYSHIFTINCREMENT uint8 = 0x01
-var LCD_ENTRYSHIFTDECREMENT uint8 = 0x00
+var lcdEntryRight uint8 = 0x00
+var lcdEntryLeft uint8 = 0x02
+var lcdEntryShiftIncrement uint8 = 0x01
+var lcdEntryShiftDecrement uint8 = 0x00
 
 // Flags for display on/off control
-var LCD_DISPLAYON uint8 = 0x04
-var LCD_DISPLAYOFF uint8 = 0x00
-var LCD_CURSORON uint8 = 0x02
-var LCD_CURSOROFF uint8 = 0x00
-var LCD_BLINKON uint8 = 0x01
-var LCD_BLINKOFF uint8 = 0x00
+var lcdDisplayOn uint8 = 0x04
+var lcdDisplayOff uint8 = 0x00
+var lcdCursorOn uint8 = 0x02
+var lcdCursorOff uint8 = 0x00
+var lcdBlinkOn uint8 = 0x01
+var lcdBlinkOff uint8 = 0x00
 
 // Flags for display/cursor shift
-var LCD_DISPLAYMOVE uint8 = 0x08
-var LCD_CURSORMOVE uint8 = 0x00
-var LCD_MOVERIGHT uint8 = 0x04
-var LCD_MOVELEFT uint8 = 0x00
+var lcdDisplayMove uint8 = 0x08
+var lcdCursorMove uint8 = 0x00
+var lcdMoveRight uint8 = 0x04
+var lcdMoveLeft uint8 = 0x00
 
 // Flags for function set
-var LCD_8BITMODE uint8 = 0x10
-var LCD_4BITMODE uint8 = 0x00
-var LCD_2LINE uint8 = 0x08
-var LCD_1LINE uint8 = 0x00
-var LCD_5x10DOTS uint8 = 0x04
-var LCD_5x8DOTS uint8 = 0x00
+var lcd8BitMode uint8 = 0x10
+var lcd4BitMode uint8 = 0x00
+var lcd2Line uint8 = 0x08
+var lcd1Line uint8 = 0x00
+var lcd5x10Dots uint8 = 0x04
+var lcd5x8Dots uint8 = 0x00
 
 // Flags for backlight control
-var LCD_BACKLIGHT uint8 = 0x08
-var LCD_NOBACKLIGHT uint8 = 0x00
+var lcdBacklight uint8 = 0x08
+var lcdNoBacklight uint8 = 0x00
 
 // Modes
-var En uint8 = 0b00000100 // Enable bit
-var Rw uint8 = 0b00000010 // Read/Write bit
-var Rs uint8 = 0b00000001 // Register select bit
+var enableBit uint8 = 0b00000100         // Enable bit
+var readWriteBit uint8 = 0b00000010      // Read/Write bit
+var registerSelectBit uint8 = 0b00000001 // Register select bit
 
 // Get our i2c connection
 func Initialize() *i2c.I2C {
-	connection, err := i2c.NewI2C(ADDRESS, I2CBUS)
+	connection, err := i2c.NewI2C(i2cAddress, i2cBus)
 	if err != nil {
 		log.Fatal("Could not initialize i2c device:", err)
 	}
@@ -102,7 +102,7 @@ func LcdDisplayString(connection *i2c.I2C, str string, line uint8, pos uint8) {
 	lcdWrite(connection, 0x80+posNew, 0)
 
 	for i := 0; i < len(str); i++ {
-		lcdWrite(connection, str[i], Rs)
+		lcdWrite(connection, str[i], registerSelectBit)
 	}
 }
 
@@ -113,22 +113,22 @@ func Reset(connection *i2c.I2C) {
 	lcdWrite(connection, 0x03, 0)
 	lcdWrite(connection, 0x02, 0)
 
-	lcdWrite(connection, LCD_FUNCTIONSET|LCD_2LINE|LCD_5x8DOTS|LCD_4BITMODE, 0)
-	lcdWrite(connection, LCD_DISPLAYCONTROL|LCD_DISPLAYON, 0)
-	lcdWrite(connection, LCD_CLEARDISPLAY, 0)
-	lcdWrite(connection, LCD_ENTRYMODESET|LCD_ENTRYLEFT, 0)
+	lcdWrite(connection, lcdFunctionSet|lcd2Line|lcd5x8Dots|lcd4BitMode, 0)
+	lcdWrite(connection, lcdDisplayControl|lcdDisplayOn, 0)
+	lcdWrite(connection, lcdClearDisplay, 0)
+	lcdWrite(connection, lcdEntryModeSet|lcdEntryLeft, 0)
 	time.Sleep(1 * time.Nanosecond)
 }
 
 // Clear lcd and set to home
 func Clear(connection *i2c.I2C) {
-	lcdWrite(connection, LCD_CLEARDISPLAY, 0)
-	lcdWrite(connection, LCD_RETURNHOME, 0)
+	lcdWrite(connection, lcdClearDisplay, 0)
+	lcdWrite(connection, lcdReturnHome, 0)
 }
 
 // Turn the display off
 func TurnOff(connection *i2c.I2C) {
-	_ = writeCmd(connection, LCD_NOBACKLIGHT)
+	_ = writeCmd(connection, lcdNoBacklight)
 }
 
 // Write a single command
@@ -147,15 +147,15 @@ func writeCmd(connection *i2c.I2C, cmd uint8) int {
 
 // Clocks EN to latch command
 func lcdStrobe(connection *i2c.I2C, data uint8) {
-	writeCmd(connection, data|En|LCD_BACKLIGHT)
+	writeCmd(connection, data|enableBit|lcdBacklight)
 	time.Sleep(1 * time.Nanosecond)
-	writeCmd(connection, ((data & ^En) | LCD_BACKLIGHT))
+	writeCmd(connection, ((data & ^enableBit) | lcdBacklight))
 	time.Sleep(1 * time.Nanosecond)
 }
 
 // Write four bits
 func lcdWriteFourBits(connection *i2c.I2C, data uint8) {
-	writeCmd(connection, data|LCD_BACKLIGHT)
+	writeCmd(connection, data|lcdBacklight)
 	lcdStrobe(connection, data)
 }
 
