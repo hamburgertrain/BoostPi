8e89a6878edf3353da1b0c6be87e36f30db57a93
Tyler Bialoblocki <hamburgertrain@users.noreply.github.com>
2024-01-22 16:26:00 -0500

    Rename device connections

--- main.go
+++ main.go
@@ -26,41 +26,41 @@ import (
 // Application entrypoint
 func main() {
 	log.Println("Initializing connection to i2c display...")
-	i2cConnection := display.Initialize()
-	defer i2cConnection.Close()
+	i2cDevice := display.Initialize()
+	defer i2cDevice.Close()
 	log.Println("Connection initialized")
 
 	// Let's make sure we have blank slate
-	display.Reset(i2cConnection)
+	display.Reset(i2cDevice)
 
 	// Show loading text while contacting ELM327 device
-	display.ShowLoadingText(i2cConnection)
+	display.ShowLoadingText(i2cDevice)
 
 	log.Println("Initializing connection to ELM327 device...")
-	dev, err := elm327.Initialize()
+	obdDevice, err := elm327.Initialize()
 	if err != nil {
-		display.ShowErrorAndShutdown(i2cConnection)
+		display.ShowErrorAndShutdown(i2cDevice)
 		log.Fatal("Could not initialize ELM327 device:", err)
 	}
 	log.Println("Connection initialized")
 
 	// Initial probe of device
-	version, err := elm327.GetVersion(dev)
+	version, err := elm327.GetVersion(obdDevice)
 	if err != nil {
-		display.ShowErrorAndShutdown(i2cConnection)
+		display.ShowErrorAndShutdown(i2cDevice)
 		log.Fatal("Error getting version:", err)
 	}
 	log.Println("Device has version:", version)
 
 	// Clear our display of loading text before showing boost
-	display.Clear(i2cConnection)
+	display.Clear(i2cDevice)
 
 	// log.Println("Simulating boost...")
-	// utilities.SimulateBoost(i2cConnection, 30)
+	// utilities.SimulateBoost(i2cDevice, 30)
 
 	// Show that we can fetch information and display it
-	utilities.GetAndDisplayValues(i2cConnection, dev)
+	utilities.GetAndDisplayValues(i2cDevice, obdDevice)
 
 	log.Println("Turning display off")
-	display.ShutdownDisplay(i2cConnection)
+	display.ShutdownDisplay(i2cDevice)
 }
